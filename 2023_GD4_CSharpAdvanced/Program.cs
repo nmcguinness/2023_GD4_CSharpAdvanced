8dbde53bed67fa07f14ad80f0e0f5b107094f9aa
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-10-26 13:13:09 +0100

    :sparkles: Added handler

--- 2023_GD4_CSharpAdvanced/Program.cs
+++ 2023_GD4_CSharpAdvanced/Program.cs
@@ -11,20 +11,23 @@ namespace _2023_GD4_CSharpAdvanced
 
         private void Start()
         {
+            //made by adding to scene hierarchy, get access to player
             Player p = new Player(5);
 
+            //in a manager (or entity) will register interest
             p.OnAbilityUpdate += HandlePlayerAbilityChange;
 
+            //happens inside Player based on some change (OnCollisionEnter)
             p.IncreaseAbility();
 
+            //in a manager when the manager is destroyed
             p.OnAbilityUpdate -= HandlePlayerAbilityChange;
         }
 
-        private void HandlePlayerAbilityChange()s
-
+        private void HandlePlayerAbilityChange()
         {
             Console.Beep(1000, 2000);
             Console.WriteLine("HandlePlayerAbilityChange...");
         }
-}
+    }
 }
