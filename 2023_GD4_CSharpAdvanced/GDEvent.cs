0e09a63255fe4f668cadaf7e0158cb4d5b915597
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-10-26 14:05:16 +0100

    :construction: Adding GDEventParams

--- 2023_GD4_CSharpAdvanced/GDEvent.cs
+++ 2023_GD4_CSharpAdvanced/GDEvent.cs
@@ -1,5 +1,27 @@
 ﻿namespace _2023_GD4_CSharpAdvanced
 {
+    public class GDEventParams<P>
+    {
+        public delegate void EventHandler(P data);
+
+        public event EventHandler OnEvent;
+
+        public void Invoke(P data)
+        {
+            OnEvent?.Invoke(data);
+        }
+
+        public void Register(EventHandler handler)
+        {
+            OnEvent += handler;
+        }
+
+        public void Deregister(EventHandler handler)
+        {
+            OnEvent -= handler;
+        }
+    }
+
     public class GDEvent
     {
         public delegate void EventHandler();
