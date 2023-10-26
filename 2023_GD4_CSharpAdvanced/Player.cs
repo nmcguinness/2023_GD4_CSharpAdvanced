54fa2175eaf258d47a40da2f0087d185f783e3da
Niall McGuinness <niall.mcguinness@dkit.ie>
2023-10-26 13:07:39 +0100

    :tada: Initial commit

--- 2023_GD4_CSharpAdvanced/Player.cs
+++ 2023_GD4_CSharpAdvanced/Player.cs
@@ -2,5 +2,23 @@
 {
     public class Player
     {
+        //list storing function references
+        public delegate void AbilityUpdateHandler();
+
+        //a flag attached to a list that stores references to subscribed functions
+        public event AbilityUpdateHandler OnAbilityUpdate;
+
+        private int abilityLevel;
+
+        public Player(int abilityLevel)
+        {
+            this.abilityLevel = abilityLevel;
+        }
+
+        public void IncreaseAbility()
+        {
+            abilityLevel++;
+            OnAbilityUpdate?.Invoke();
+        }
     }
 }
--- 2023_GD4_CSharpAdvanced/Program.cs
+++ 2023_GD4_CSharpAdvanced/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace _2023_GD4_CSharpAdvanced
+﻿using System;
+
+namespace _2023_GD4_CSharpAdvanced
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            new Program().Start();
+        }
+
+        private void Start()
+        {
+            throw new NotImplementedException();
         }
     }
 }
